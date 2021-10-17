4442500931cf7fdb0093a255d920bd195680631c
Maxi Conde <maxicondep@gmail.com>
2021-10-17 18:37:12 -0300

    Se agrega ItemCount

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css';
+import ItemCount from './components/ItemCount/ItemCount';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import {Navbar} from './components/Navbar/Navbar'
 
+
+
+
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
         <Navbar/>
       </header>
-      <ItemListContainer message="greeting"/>
+      <ItemListContainer message="greeting" children= {<ItemCount stock="5"  initial="0"/>}/>
     </div>
   );
 }
